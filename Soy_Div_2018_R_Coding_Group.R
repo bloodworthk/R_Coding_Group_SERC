b753e01ee24e11e0e2ca9685a4b5129c18e20733
Kathryn Bloodworth <bloodworthk@si.edu>
2019-01-17 11:12:25 -0500

    Original Push to GitHub

---
+++ Soy_Div_2018_R_Coding_Group.R
@@ -0,0 +1,10 @@
+#### Soy_Div_2018 data analysis with R Coding Group at SERC ####
+
+#### Set Working Directories ####
+
+#Kathryn -WD
+#PC 
+setwd("/Users/bloodworthk/Dropbox (Smithsonian)/SERC Ecosystem Conservation/Projects/Common_Garden/2018/Data")
+#Mac
+setwd("/Users/kathrynbloodworth/Dropbox (Smithsonian)/SERC Ecosystem Conservation/projects/Common_Garden/2018/Data")
+
