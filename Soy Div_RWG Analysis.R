b3c224a2c9336a68ab07a8d72d9dc84aa66c01ff
Kathryn Bloodworth <bloodworthk@si.edu>
2019-01-17 15:01:01 -0500

    Set working directory

--- Soy Div_RWG Analysis.R
+++ Soy Div_RWG Analysis.R
@@ -8,6 +8,8 @@
 setwd("C:/Users/Benton/Desktop/Work Files/Courses Taught/R Study Group_SERC/Soy Div_R_Coding_Group_SERC")
 
 #Kathryn WD
+setwd("~/Dropbox (Smithsonian)/SERC Ecosystem Conservation/R_Coding_Working_Group/Soy_Div_2018_R_Coding_Group")
+
 
 #Kristina WD
 
