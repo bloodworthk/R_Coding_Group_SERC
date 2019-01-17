00aa3fc61a5ee4f076baa81ddc91859f25c15e24
Kathryn Bloodworth <bloodworthk@si.edu>
2019-01-17 11:13:18 -0500

    Installing and downloading packages

--- Soy_Div_2018_R_Coding_Group.R
+++ Soy_Div_2018_R_Coding_Group.R
@@ -8,3 +8,26 @@ setwd("/Users/bloodworthk/Dropbox (Smithsonian)/SERC Ecosystem Conservation/Proj
 #Mac
 setwd("/Users/kathrynbloodworth/Dropbox (Smithsonian)/SERC Ecosystem Conservation/projects/Common_Garden/2018/Data")
 
+#### Soy_Div_2018 data analysis with R Coding Group at SERC ####
+
+#### Set Working Directories ####
+
+#Kathryn -WD
+#PC 
+setwd("/Users/bloodworthk/Dropbox (Smithsonian)/SERC Ecosystem Conservation/Projects/Common_Garden/2018/Data")
+#Mac
+setwd("/Users/kathrynbloodworth/Dropbox (Smithsonian)/SERC Ecosystem Conservation/projects/Common_Garden/2018/Data")
+
+#### Install and Download Packages ####
+
+#install.packages("tidyverse")
+library(tidyverse)
+#install.packages("readxl")
+library(readxl)
+#install.packages("summarytools")
+library(summarytools)
+#install.packages("lme4")
+library(lme4)
+#install.packages("lmerTest")
+library(lmerTest)
+
