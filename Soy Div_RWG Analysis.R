2c91c46cc24bb23351cb8bbf2fb996af495d75e5
Benton Taylor <bentonneiltaylor@gmail.com>
2019-01-17 14:53:41 -0500

    checking to see if things are connected in github

--- Soy Div_RWG Analysis.R
+++ Soy Div_RWG Analysis.R
@@ -1,5 +1,13 @@
 #### Soy Div Working Group Analysis ####
+#Coders: Kathryn Bloodworth, Kristina Borst, Ben Taylor
+#Purspose: Learning Stuff about Soybeans
 
+#### Working Directories ####
 
+#Ben WD
+setwd("C:/Users/Benton/Desktop/Work Files/Courses Taught/R Study Group_SERC/Soy Div_R_Coding_Group_SERC")
 
-#### Setting up Working Directory ####
+#Kathryn WD
+
+#Kristina WD
+
